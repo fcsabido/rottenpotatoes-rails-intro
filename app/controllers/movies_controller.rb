2465bdb80f1f88b1222f3c6bb9f8151274901426
EC2 Default User <ec2-user@ip-172-31-7-51.ec2.internal>
2021-02-22 21:46:04 +0000

    Adding checkboxes-(testing).

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,15 +11,7 @@ class MoviesController < ApplicationController
     @all_ratings = Movie.all_ratings
     @ratings_to_show = params[:ratings] || session[:ratings] || {}
     
-    if @ratings_to_show == {}
-      return @movies =  Movie.all
-    elsif !params[:ratings].nil?
-      return @movies = Movie.where(rating: (params[:ratings]).keys)
-    #elsif !session[:ratings].nil?
-    #  return @movies = Movie.where(rating: (session[:ratings]).keys)
-    end
-      
-      
+    @movies = Movie.with_ratings(params[:ratings])  
     
   end
 
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,10 +5,17 @@ class Movie < ActiveRecord::Base
   end
   
   def self.with_ratings(ratings_list)
-  # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
-  #  movies with those ratings
-  
+
   # if ratings_list is nil, retrieve ALL movies
+  if ratings_list.nil?
+    return @movies =  Movie.all
+  # if ratings_list is a hasg, retrieve all movies using 
+  # its keys
+  else
+    return @movies = Movie.where(rating: rating_list.keys)
+  end
+    
+  
   end
 
 end
