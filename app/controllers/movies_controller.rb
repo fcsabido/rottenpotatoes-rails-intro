6dd843a2a5efdbcbd5767d1af4b4af51faf17cca
EC2 Default User <ec2-user@ip-172-31-7-51.ec2.internal>
2021-02-24 06:32:36 +0000

    Part-3 complete.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -16,24 +16,24 @@ class MoviesController < ApplicationController
     # :home is generated when the user click either the Refresh button or any sorting column. Then, 
     # we have the following paths:
     # :home does not exists in neither params nor session => Fresh page
-    # :home exists in params => User still in movies page
+    # :home exists in params => User still in movies page, save session
     # :home does not exists in params, but exists in session => User is returning after setting params,
     #                                                        redirect to a page with loaded session
     #                                                        parameters and erase session
-    if !params[:home].nil?
-      session[:home] = params[:home]
-      session[:sort] = params[:sort] if !params[:sort].nil?
-      session[:ratings] = params[:ratings] if !params[:ratings].nil?
-      session[:title_sort] = params[:title_sort] if !params[:title_sort].nil?
-      session[:release_date_sort] = params[:release_date_sort] if !params[:release_date_sort].nil?
-    elsif !session[:home].nil?
-    #  params[:homeTwo] = "1" 
+    if params[:home].nil? && !session[:home].nil? #|| !params[:homeTwo].nil?
+      params[:home] = session[:home] if !session[:home].nil?
       params[:sort] = session[:sort] if !session[:sort].nil?
       params[:ratings] = session[:ratings] if !session[:ratings].nil?
       params[:title_sort] = session[:title_sort] if !session[:title_sort].nil?
       params[:release_date_sort] = session[:release_date_sort] if !session[:release_date_sort].nil?
       session.clear
       redirect_to movies_path({:params => params}) and return
+    elsif !params[:home].nil?
+      session[:home] = params[:home]
+      session[:sort] = params[:sort] if !params[:sort].nil?
+      session[:ratings] = params[:ratings] if !params[:ratings].nil?
+      session[:title_sort] = params[:title_sort] if !params[:title_sort].nil?
+      session[:release_date_sort] = params[:release_date_sort] if !params[:release_date_sort].nil?
     end
     
     @ratings_to_show = params[:ratings] || {}
